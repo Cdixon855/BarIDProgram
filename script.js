0ebeeccf6eae18cd7ebab9089894ef61e222162a
Jordan Brandes <jordanbrandes@yahoo.com>
2018-12-01 12:00:39 -0600

    Create script.js
    The foundation of where I want the program to go. The age will be pulled from a birthYear function which will be pulled from OpenCV.

---
+++ script.js
@@ -0,0 +1,4 @@
+var firstName = prompt('what is your first name?');
+var age = 28
+
+age >= 21 ? console.log(firstName+ ' step inside please') : console.log(firstName + ' I/m sorry you/re too young');
