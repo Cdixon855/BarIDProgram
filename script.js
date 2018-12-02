2215e3a28ecec99cb4ce931f7907c6f2d7514e2a
Jordan Brandes <jordanbrandes@yahoo.com>
2018-12-02 10:08:01 -0600

    Update script.js
    Created new age prompt

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var firstName = prompt('what is your first name?');
-var age = 28
+var age = prompt('How old are you?');
+
 
 age >= 21 ? console.log(firstName+ ' step inside please') : console.log(firstName + ' I/m sorry you/re too young');
